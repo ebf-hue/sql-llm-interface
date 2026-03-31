1d2b052e0f1adc0f36d48638f0cb0111b2c9a448
ebf-hue <ebfi@pm.me>
2026-03-31 18:35:18 -0400

    add to file structure

--- modules/sqlite_setup.py
+++ modules/sqlite_setup.py
@@ -1,1 +1,9 @@
 # sqlite_setup.py
+
+import sqlite3
+
+# get the db connection
+def get_connection(db_path: str) -> sqlite3.Connection:
+    conn = sqlite3.connect(db_path)
+    conn.row_factory = sqlite3.Row
+    return conn
